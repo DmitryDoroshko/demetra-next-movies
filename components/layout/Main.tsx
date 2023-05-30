11d335a31ca9f37f5a039e95279e63f646389d36
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-05-30 20:37:39 +0300

    Fix the formatting in Main.tsx

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,27 +1,28 @@
-import {Grid} from "@mui/material";
-import {Roboto} from "next/font/google";
+import { Grid } from "@mui/material";
+import { Roboto } from "next/font/google";
 import React from "react";
 
 const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
-export default function Main({children}: {children: React.ReactNode}) {
+export default function Main({ children }: { children: React.ReactNode }) {
   return (
-      <main className={roboto.className}>
-        <Grid
-            container
-            spacing={0}
-            marginTop={"5rem"}
-            direction={"column"}
-            alignItems={"center"}
-            justifyContent={"flex-start"}
-            style={{minHeight: "100vh"}}>
-          <Grid item xs={3}>
-            {children}
-          </Grid>
+    <main className={roboto.className}>
+      <Grid
+        container
+        spacing={0}
+        marginTop={"5rem"}
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"flex-start"}
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          {children}
         </Grid>
-      </main>
+      </Grid>
+    </main>
   );
 }

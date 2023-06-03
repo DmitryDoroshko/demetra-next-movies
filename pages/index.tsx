04e472894841834994d09259f2ce0a02f23f904a
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-06-03 14:58:10 +0300

    Remove the localStorage calls from server pages.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,7 @@
 import {Button} from "@mui/material";
 import Link from "next/link";
-import { useAppDispatch } from "@/hooks/redux-hooks";
-import { useEffect } from "react";
-import { extractFavoriteMoviesFromLocalStorage } from "@/helpers/local-storage";
-import { setFavoriteMovies } from "@/store/favoriteMovies/favoriteMoviesSlice";
 
 export default function Home() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const favoriteMovies = extractFavoriteMoviesFromLocalStorage();
-    dispatch(setFavoriteMovies(favoriteMovies));
-  }, []);
-
   return (
       <>
         <h1>
